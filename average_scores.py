2ece7469e6ca80b02c2276652c5f78e824357888
Sam-G-23 <64095339+Sam-G-23@users.noreply.github.com>
2020-06-09 17:38:24 -0500

    Add files via upload

--- average_scores.py
+++ average_scores.py
@@ -2,21 +2,39 @@
 This supposed to take 3 inputs convert to an int and spit out an average score
 Sam Goode sgoode1@dmacc.edu 6/4/2020
 """
-last_name = input("What is your last name? ")
-first_name = input("What is your first name? ")
-age = input("How old are you? ")
-test1 = int(input("Enter score for your first test: "))
-test2 = int(input("Enter score for your second test: "))
-test3 = int(input("Enter score for your third test: "))
-total = (test1 + test2 + test3)
 # Inputs for the user and total to simplify the function below
-def average(total):
-    return total / 3
+test1 = 0
+test2 = 0
+test3 = 0
+
+
+def average(test1, test2, test3):
+    input("Please input your full name. ")
+    test1 = int(input("Now your first test grade. "))
+    if (test1 >= 0) and (test1 <= 100):
+        try:
+            print("Thank you.")
+        except:
+            print("Invalid input please try again. Try again>")
+    test2 = int(input("Now your second test. "))
+    if (test2 >= 0) and (test2 <= 100):
+        try:
+            print("Thank you.")
+        except:
+            print("Invalid input please try again. Try again>")
+    test3 = int(input("Now your second test. "))
+    if (test3 >= 0) and (test3 <= 100):
+        try:
+            print("Thank you.")
+        except:
+            print("Invalid input please try again. Try again>")
+    final_avg_test = (test1 + test2 + test3) / 3
+    return average(test1 + test2 + test3)
 # Calculating average
 
 
-average_scores = average(total)
-print(last_name, ",", first_name, age, "the average score of your tests are:", average(total))
+if __name__ == "__main__":
+    print("Congrats, your average test score is:", average(test1, test2, test3))
 # Print out the test score
 
 
--- test_average_scores.py
+++ test_average_scores.py
@@ -1,13 +1,23 @@
+"""
+This is supposed to test average_scores.py
+Sam Goode sgoode1@dmacc.edu 6/4/2020
+"""
 import unittest
 import unittest.mock as mock
 from format_output import average_scores as avg
-
+# importing from the other file for this project
 
 class MyTestCase(unittest.TestCase):
     def test_average(self):
         with mock.patch('builtins.input', average=[85,90,95]):
-            assert avg.average(total=270) == 90
-
+            self.assertAlmostEqual(avg.average, 90, places=4)
+    pass
+# So I was not 100% sure if the assert argument was right or not but the test is passing
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            avg.average()
 
 if __name__ == '__main__':
     unittest.main()
+# I am lost.
+# I have no idea what I am doing with this test assert
